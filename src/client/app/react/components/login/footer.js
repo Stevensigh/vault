49776264ea139416fd2484881bee929590b508d8
Kevin Lin <developer@kevinlin.info>
2018-03-05 13:27:38 -0600

    Login components

---
+++ src/client/app/react/components/login/footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Spacing, Text } from 'react-elemental';
+
+const {
+  FOOTER_TEXT = '',
+} = process.env;
+
+/**
+ * Footer displayed in the login page.
+ */
+const Footer = () => (
+  <footer>
+    <Spacing style={{ display: 'flex', justifyContent: 'space-between' }} top bottom>
+      <div>
+        <Text color="gray80" size="kilo" bold inline>
+          Vault&nbsp;
+        </Text>
+        <Text color="gray80" size="kilo" inline>
+          is an open source secrets and credentials manager.
+        </Text>
+      </div>
+
+      <Text color="gray60" size="kilo">
+        {FOOTER_TEXT}
+      </Text>
+    </Spacing>
+  </footer>
+);
+
+export default Footer;
---
+++ src/client/app/react/components/login/password-field.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { colors, sizes, Button, Spacing, Text, TextField } from 'react-elemental';
+import LongArrowRight from 'client/app/react/components/ui/icon/long-arrow-right';
+
+/**
+ * Field for entering the master decryption password.
+ */
+const PasswordField = ({ value, error, onChange, onSubmit }) => (
+  <form style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+    <Spacing size="large" bottom>
+      <Text size="delta" color="gray20">
+        Enter the master decryption password to continue.
+      </Text>
+    </Spacing>
+
+    <TextField
+      type="password"
+      value={value}
+      onChange={onChange}
+      error={error}
+      style={{
+        color: colors.gray10,
+        fontSize: sizes.beta,
+      }}
+      secondary
+    />
+
+    <Spacing size="large" style={{ alignSelf: 'flex-end' }} top>
+      <Button
+        type="submit"
+        onClick={onSubmit}
+        style={{
+          backgroundColor: 'inherit',
+          marginRight: '-16px',
+        }}
+      >
+        <LongArrowRight style={{ fill: colors.gray10, width: '25px' }} />
+      </Button>
+    </Spacing>
+  </form>
+);
+
+PasswordField.propTypes = {
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+PasswordField.defaultProps = {
+  error: null,
+};
+
+export default PasswordField;
