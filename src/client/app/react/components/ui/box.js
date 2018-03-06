1527f1e67891617c2c45dc5d88796ad4d519fb38
Kevin Lin <developer@kevinlin.info>
2018-03-06 10:25:25 -0600

    Box shadows

--- src/client/app/react/components/ui/box.js
+++ src/client/app/react/components/ui/box.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Spacing } from 'react-elemental';
+import withToggleState from 'client/app/react/hoc/with-toggle-state';
 
 /**
  * UI wrapper component for segmenting multiple children nodes into a single visual block.
  */
-const Box = ({ children, ...props }) => (
+const Box = ({ isHover, handleMouseEnter, handleMouseLeave, children, ...props }) => (
   <Spacing
-    style={{ backgroundColor: 'rgb(18, 18, 18)', borderRadius: '3px' }}
+    style={{
+      backgroundColor: 'rgb(18, 18, 18)',
+      borderRadius: '3px',
+      boxShadow: `4px 4px 10px 0px rgba(0, 0, 0, ${isHover ? 0.17 : 0.1})`,
+      transition: 'all 0.25s ease',
+    }}
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
     top
     right
     bottom
@@ -20,7 +28,14 @@ const Box = ({ children, ...props }) => (
 );
 
 Box.propTypes = {
+  isHover: PropTypes.bool.isRequired,
+  handleMouseEnter: PropTypes.func.isRequired,
+  handleMouseLeave: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
 
-export default Box;
+export default withToggleState({
+  key: 'isHover',
+  enable: 'handleMouseEnter',
+  disable: 'handleMouseLeave',
+})(Box);
