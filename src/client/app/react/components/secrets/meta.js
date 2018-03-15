536041f0da844c3f0bef1cd90cbb66ab615728fe
Kevin Lin <developer@kevinlin.info>
2018-03-15 15:36:36 -0500

    Secrets metadata component

---
+++ src/client/app/react/components/secrets/meta.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Text } from 'react-elemental';
+
+/**
+ * Header in the secrets listing describing metadata about the fetched secrets.
+ */
+const SecretsMeta = ({ numSecrets, onAddClick }) => (
+  <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between' }}>
+    <div>
+      <Text color="gray50" bold inline>
+        {numSecrets}
+      </Text>
+      <Text color="gray60" inline>
+        &nbsp;matching secrets
+      </Text>
+    </div>
+
+    <div>
+      <Button
+        size="gamma"
+        text="Add new"
+        onClick={onAddClick}
+      />
+    </div>
+  </div>
+);
+
+SecretsMeta.propTypes = {
+  numSecrets: PropTypes.number.isRequired,
+  onAddClick: PropTypes.func.isRequired,
+};
+
+export default SecretsMeta;
