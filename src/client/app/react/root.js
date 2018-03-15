f5491d5d6336fc76332c37c76266a640c2936e90
Kevin Lin <developer@kevinlin.info>
2018-03-15 16:33:34 -0500

    Move background color responsibility to HTML body tag

--- src/client/app/react/root.js
+++ src/client/app/react/root.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { colors } from 'react-elemental';
 import routes from 'client/app/react/routes';
 
 /**
@@ -7,18 +6,6 @@ import routes from 'client/app/react/routes';
  */
 const Root = () => (
   <div>
-    <div
-      style={{
-        backgroundColor: colors.gray95,
-        height: '100%',
-        left: 0,
-        position: 'fixed',
-        top: 0,
-        width: '100%',
-        zIndex: -1,
-      }}
-    />
-
     {routes}
   </div>
 );
