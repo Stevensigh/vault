460f0cfbfb6a073a252fdf949029888108b0a485
Kevin Lin <developer@kevinlin.info>
2018-03-16 14:20:13 -0500

    MySQL database client

---
+++ src/server/clients/db.js
@@ -0,0 +1,30 @@
+import mysql from 'mysql';
+
+/**
+ * Proxying client to MySQL.
+ */
+export default class DBClient {
+  /**
+   * Create a MySQL client instance.
+   *
+   * @param {String} host Hostname of the database.
+   * @param {Number} port Port of the database.
+   * @param {String} name Database name.
+   * @param {String} user Database username.
+   * @param {String} pass Corresponding database user password.
+   */
+  constructor({ host, port, name, user, pass }) {
+    this.conn = mysql.createConnection({
+      host,
+      port,
+      user,
+      password: pass,
+      database: name,
+    });
+    this.conn.connect();
+  }
+
+  exec(...args) {
+    return this.conn.query(...args);
+  }
+}
--- src/server/context.js
+++ src/server/context.js
@@ -1,14 +1,27 @@
 import fs from 'fs';
 import CryptographyClient from 'server/clients/cryptography';
+import DBClient from 'server/clients/db';
 
 const {
   SALT_FILE_PATH = '.salt',
+  DB_HOST = 'localhost',
+  DB_PORT = 3306,
+  DB_NAME = 'vault',
+  DB_USER = 'vault',
+  DB_PASS = '',
 } = process.env;
 
 export default class Context {
   constructor() {
     this.crypto = new CryptographyClient({
       salt: fs.readFileSync(SALT_FILE_PATH).toString('utf8'),
+    });
+    this.db = new DBClient({
+      host: DB_HOST,
+      port: DB_PORT,
+      name: DB_NAME,
+      user: DB_USER,
+      pass: DB_PASS,
     });
   }
 }
