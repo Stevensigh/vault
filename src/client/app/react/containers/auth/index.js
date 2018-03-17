0ed62c9682ab8e3594240f9e27ea3336d96b19ce
Kevin Lin <developer@kevinlin.info>
2018-03-17 15:33:28 -0500

    Auth container validates login state

--- src/client/app/react/containers/auth/index.js
+++ src/client/app/react/containers/auth/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router';
+import { Redirect } from 'react-router-dom';
 import { Spacing } from 'react-elemental';
+import { withResource } from 'supercharged/client';
 import AdminContainer from 'client/app/react/containers/admin';
 import SecretsContainer from 'client/app/react/containers/secrets';
 import Header from 'client/app/react/components/header';
 import Container from 'client/app/react/components/ui/container';
+import Splash from 'client/app/react/components/ui/splash';
+import { CODE_SESSION_EXPIRED, CODE_SESSION_UNAUTHENTICATED } from 'shared/constants/error';
 
 const ROUTE_HEADER_TABS = [
   { route: '/admin', tab: 'admin' },
@@ -15,7 +19,7 @@ const ROUTE_HEADER_TABS = [
 /**
  * Container for mediating routing changes between different selected tabs.
  */
-export default class AuthContainer extends Component {
+class AuthContainer extends Component {
   static propTypes = {
     history: PropTypes.shape({
       push: PropTypes.func.isRequired,
@@ -23,30 +27,55 @@ export default class AuthContainer extends Component {
     match: PropTypes.shape({
       path: PropTypes.string.isRequired,
     }).isRequired,
+    auth: PropTypes.shape({
+      isLoaded: PropTypes.bool.isRequired,
+      err: PropTypes.shape({
+        code: PropTypes.number.isRequired,
+      }),
+    }).isRequired,
   };
 
   handleTabChange = (tab) =>
     this.props.history.push(ROUTE_HEADER_TABS.find((entry) => entry.tab === tab).route);
 
   render() {
-    const { match: { path } } = this.props;
+    const {
+      match: { path },
+      auth: { isLoaded, err },
+    } = this.props;
+
+    if (err && [CODE_SESSION_UNAUTHENTICATED, CODE_SESSION_EXPIRED].includes(err.code)) {
+      return (
+        <Redirect to={{ pathname: '/login', state: { code: err.code } }} />
+      );
+    }
 
     const { tab } = ROUTE_HEADER_TABS.find(({ route }) => route === path);
 
     return (
-      <Container>
-        <Spacing size="large" bottom>
-          <Header
-            tab={tab}
-            onChange={this.handleTabChange}
-          />
-        </Spacing>
+      <div>
+        <Splash show={!isLoaded} />
 
-        <Switch>
-          <Route path="/admin" component={AdminContainer} exact />
-          <Route path="/secrets" component={SecretsContainer} exact />
-        </Switch>
-      </Container>
+        <Container>
+          <Spacing size="large" bottom>
+            <Header
+              tab={tab}
+              onChange={this.handleTabChange}
+            />
+          </Spacing>
+
+          <Switch>
+            <Route path="/admin" component={AdminContainer} exact />
+            <Route path="/secrets" component={SecretsContainer} exact />
+          </Switch>
+        </Container>
+      </div>
     );
   }
 }
+
+export default withResource({
+  key: 'auth',
+  method: 'GET',
+  endpoint: '/api/login/status',
+})(AuthContainer);
