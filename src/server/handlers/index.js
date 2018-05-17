7e16f02420fd308462835383d8390cee3759ecc5
Kevin Lin <developer@kevinlin.info>
2018-05-17 16:27:14 -0500

    Register all login handlers

--- src/server/handlers/index.js
+++ src/server/handlers/index.js
@@ -1,7 +1,13 @@
 import ErrorHandler from 'server/handlers/error';
 import FrontendHandler from 'server/handlers/frontend';
+import LoginVerifyHandler from 'server/handlers/login/verify';
+import LoginPasswordHandler from 'server/handlers/login/password';
+import LoginStatusHandler from 'server/handlers/login/status';
 
 export default [
+  LoginVerifyHandler,
+  LoginPasswordHandler,
+  LoginStatusHandler,
   FrontendHandler,
   ErrorHandler,
 ];
