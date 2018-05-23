cd03a158f2d6cffa6cf8cc23da5664d9c849be2f
Kevin Lin <developer@kevinlin.info>
2018-05-23 09:11:07 -0500

    Move schema SQL into dedicated file

--- src/server/scripts/init-db.js
+++ src/server/scripts/init-db.js
@@ -1,27 +1,14 @@
 /* eslint-disable no-console */
 
+import fs from 'fs';
+import path from 'path';
 import Context from 'server/context';
-
-const CREATE_TABLES_SQL = `
-  CREATE TABLE IF NOT EXISTS secret (
-      \`id\` INT(11) NOT NULL AUTO_INCREMENT,
-      \`protected\` TINYINT(1) NOT NULL DEFAULT 0,
-      \`timestamp\` BIGINT(11) NOT NULL,
-      \`name\` VARCHAR(255) NOT NULL,
-      \`identity\` TEXT DEFAULT NULL,
-      \`link\` TEXT DEFAULT NULL,
-      \`secret\` TEXT NOT NULL,
-      \`notes\` TEXT DEFAULT NULL,
-      KEY \`timestamp_idx\` (\`timestamp\`),
-      UNIQUE KEY \`name_idx\` (\`name\`),
-      PRIMARY KEY (\`id\`)
-  );
-`;
 
 const main = () => {
   const ctx = new Context();
+  const schema = fs.readFileSync(path.join(__dirname, './db/schema.sql')).toString('utf-8');
 
-  ctx.db.exec(CREATE_TABLES_SQL, (err) => {
+  ctx.db.exec(schema, (err) => {
     if (err) {
       throw err;
     }
